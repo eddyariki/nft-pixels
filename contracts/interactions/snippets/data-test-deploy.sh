c4d027db79f3f6dcdab8189936840094784eb28e
eddyariki <edoueda@gmail.com>
2021-03-16 15:26:18 +0900

    added shell commands for easy deployment of smart contracts

---
+++ contracts/interactions/snippets/data-test-deploy.sh
@@ -0,0 +1,3 @@
+tsc ../data-test/deploy.ts --outDir ../data-test/output
+echo "ts compiled"
+node ../data-test/output/deploy.js
---
+++ contracts/interactions/snippets/pixel-ownership-deploy.sh
@@ -0,0 +1,3 @@
+tsc ../pixel-ownership/deploy.ts --outDir ../pixel-ownership/output
+echo "ts compiled"
+node ../pixel-ownership/output/deploy.js

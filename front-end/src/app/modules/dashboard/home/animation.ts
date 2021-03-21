90dd833fb56b9dd06322841428ac63a423f49c6c
poteboy <keitatkins4@gmail.com>
2021-03-22 01:43:32 +0900

    see?

--- front-end/src/app/modules/dashboard/home/animation.ts
+++ front-end/src/app/modules/dashboard/home/animation.ts
@@ -12,8 +12,8 @@ import {
 export const HomeAnimation = trigger('pathSelect', [
     state('home', style({
         margin: 'auto',
-        height: '600px',
-        width: '60vw',
+        height: '500px',
+        width: '70vw',
         backgroundColor: 'rgb(255, 255, 255)',
         display: 'grid',
         alignItems: 'center',
@@ -22,7 +22,7 @@ export const HomeAnimation = trigger('pathSelect', [
     })),
     state('auction', style({
         margin: 'auto',
-        height: '700px',
+        height: '600px',
         width: '60vw',
         backgroundColor: 'rgb(255, 255, 255)',
         display: 'grid',
@@ -40,6 +40,7 @@ export const HomeAnimation = trigger('pathSelect', [
         alignItems: 'center',
         borderRadius: '40px',
         boxShadow: '0 10px 15px 0 rgba(0, 0, 0, .2)',
+
     })),
     transition('home <=> auction', [
         animate('1s')
